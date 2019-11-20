1ff2897cee8f0f99d57609066738f944be80cb9b
prkhr-agrwl <aprakhar51@gmai.com>
2019-11-20 19:42:49 +0530

    sign in button in sidebar

--- src/components/sidebar/menu.jsx
+++ src/components/sidebar/menu.jsx
@@ -1,13 +1,13 @@
 const Menu = [
   {
-    path: '/dashboard',
-    icon: 'fa fa-th',
-    title: 'Dashboard'
+    path: "/dashboard",
+    icon: "fa fa-th",
+    title: "Dashboard"
   },
   {
-    path: '/knowledgeDomains',
-    icon: 'fa fa-hdd',
-    title: 'Knowledge Domains'
+    path: "/knowledgeDomains",
+    icon: "fa fa-hdd",
+    title: "Knowledge Domains"
     // children: [
     //   { path: '/knowledgeDomains/jee', title: 'JEE' },
     //   { path: '/knowledgeDomains/advanced', title: 'Advanced' },
@@ -15,29 +15,39 @@ const Menu = [
     // ]
   },
   {
-    path: '/courses',
-    icon: 'fab fa-simplybuilt',
-    title: 'Courses'
+    path: "/signIn",
+    icon: "fa fa-hdd",
+    title: "Sign In"
+    // children: [
+    //   { path: '/knowledgeDomains/jee', title: 'JEE' },
+    //   { path: '/knowledgeDomains/advanced', title: 'Advanced' },
+    //   { path: '/knowledgeDomains/neet', title: 'NEET' }
+    // ]
   },
   {
-    path: '/ui',
-    icon: 'fa fa-gem',
-    title: 'Admin Activity'
+    path: "/courses",
+    icon: "fab fa-simplybuilt",
+    title: "Courses"
   },
   {
-    path: '/bootstrap-4',
-    icon: 'fa fa-gem',
-    title: 'Students'
+    path: "/ui",
+    icon: "fa fa-gem",
+    title: "Admin Activity"
   },
   {
-    path: '/form',
-    icon: 'fa fa-list-ol',
-    title: 'Orders'
+    path: "/bootstrap-4",
+    icon: "fa fa-gem",
+    title: "Students"
   },
   {
-    path: '/table',
-    icon: 'fa fa-table',
-    title: 'Logout'
+    path: "/form",
+    icon: "fa fa-list-ol",
+    title: "Orders"
+  },
+  {
+    path: "/table",
+    icon: "fa fa-table",
+    title: "Logout"
   }
 ];
 
